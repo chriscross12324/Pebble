b366f745d3e96ccaf29ab47d8f3d9875a877cccb
Chris <chrismc16@outlook.com>
2023-11-11 13:56:50 -0800

    - Added [ALL]; Added as placeholders for a possible re-engineering

---
+++ app/src/main/java/com/simple/chris/pebble/testing/HeroTransitionObject.kt
@@ -0,0 +1,14 @@
+package com.simple.chris.pebble.testing
+
+class HeroTransitionObject {
+    /**
+     * This would act as the 'gradientScreenAnimationHero' from activity_main.xml
+     *
+     * It handles the transition animations between multiple Activities and Fragments
+     * and would make more sense to be it's own object that spawns when needed and
+     * gets destroyed after completion.
+     *
+     * A nice addition would be if multiple could be spawned, fixing the visual
+     * glitch of it not being visible if multiple animations are trying to play.
+     */
+}
---
+++ app/src/main/java/com/simple/chris/pebble/testing/SmallScreen.kt
@@ -0,0 +1,11 @@
+package com.simple.chris.pebble.testing
+
+class SmallScreen {
+    /**
+     * This would act as the 'smallScreenFragHolder' from activity_main.xml
+     *
+     * It would display the Settings and About screens, maybe as a dialog?
+     * Making this it's own thing cleans up the logic as it would only need
+     * to be called rather than ActivityMain.kt handling all the logic.
+     */
+}
